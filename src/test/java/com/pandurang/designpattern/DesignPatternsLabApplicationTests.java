7b13e4bb835a4991495d0cbf2defb19338cf56d2
Pandurang Kapate <pandurangkapate76@gmail.com>
2026-08-13 20:33:38 +0530

    Initial commit

---
+++ src/test/java/com/pandurang/designpattern/DesignPatternsLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pandurang.designpattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignPatternsLabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
